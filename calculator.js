cc0d8f0c75004c4a287cf9cfe09120188637dd6a
Imstronk123 <89363868+Imstronk123@users.noreply.github.com>
2022-02-18 14:49:34 +0700

    Create calculator.js

---
+++ calculator.js
@@ -0,0 +1,10 @@
+const nerdamer = require('nerdamer/all.min');
+
+try {
+        if (message.body.toLowerCase().startsWith('!solve ')) {
+            var question = nerdamer(`${message.body.slice(7)}`);
+            msg.reply(`${question.toString()}`)
+        }
+    } catch(err) {
+        message.reply(`${err}`)
+    }
